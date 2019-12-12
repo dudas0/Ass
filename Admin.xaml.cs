169db4903c1fe2b38803124f327fe54d17f6fea7
dudas0 <46559945+dudas0@users.noreply.github.com>
2019-12-12 13:55:44 +0100

    Add files via upload

--- Admin.xaml.cs
+++ Admin.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +22,96 @@ namespace GUI
         public Admin()
         {
             InitializeComponent();
+            FillListView();
+            SqlConnection con = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = MA; Integrated Security = True;");
+
+            con.Open();
+//filmy
+            SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) FROM movies");
+            sqlCommand.Connection = con;
+
+            int filmy = Convert.ToInt32(sqlCommand.ExecuteScalar());
+
+            lmovies.Content = filmy.ToString();
+//użytkwnicy
+            SqlCommand sqlCommand2 = new SqlCommand("SELECT COUNT(*) FROM tableUser");
+            sqlCommand2.Connection = con;
+            int uzytkownicy = Convert.ToInt32(sqlCommand2.ExecuteScalar());
+
+            lusers.Content = uzytkownicy.ToString();
+//komedie
+            SqlCommand sqlCommand3 = new SqlCommand("SELECT COUNT(*) FROM movies WHERE genre='komedia'");
+            sqlCommand3.Connection = con;
+            int komedie = Convert.ToInt32(sqlCommand3.ExecuteScalar());
+
+            lcomedy.Content = komedie.ToString();
+//dramaty
+            SqlCommand sqlCommand4 = new SqlCommand("SELECT COUNT(*) FROM movies WHERE genre='dramat'");
+            sqlCommand4.Connection = con;
+            int dramaty = Convert.ToInt32(sqlCommand4.ExecuteScalar());
+
+            ldrama.Content = dramaty.ToString();
+//horrory
+            SqlCommand sqlCommand5 = new SqlCommand("SELECT COUNT(*) FROM movies WHERE genre='horror'");
+            sqlCommand5.Connection = con;
+            int horrory = Convert.ToInt32(sqlCommand5.ExecuteScalar());
+
+            lhorror.Content = horrory.ToString();
+//animowane
+            SqlCommand sqlCommand6 = new SqlCommand("SELECT COUNT(*) FROM movies WHERE genre='animowany'");
+            sqlCommand6.Connection = con;
+            int animowane = Convert.ToInt32(sqlCommand6.ExecuteScalar());
+
+            lanim.Content = animowane.ToString();
+
+//świąteczne
+            SqlCommand sqlCommand7 = new SqlCommand("SELECT COUNT(*) FROM movies WHERE genre='świąteczny'");
+            sqlCommand7.Connection = con;
+            int swiateczne = Convert.ToInt32(sqlCommand7.ExecuteScalar());
+
+            lchrist.Content = swiateczne.ToString();
+        }
+        private void FillListView()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = MA; Integrated Security = True;");
+            using SqlDataAdapter adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand("Select Title, Director, Year, Genre, AvgRate, NumOfRates, Watched  from movies", con)
+            };
+
+            DataTable dt = new DataTable("movies");
+            adapter.Fill(dt);
+            dg.ItemsSource = dt.DefaultView;
+
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = MA; Integrated Security = True;");
+            try
+            {
+                string query = "Insert into [movies] (Title, Director, Year, Genre, AvgRate, NumOfRates, Watched) values ('" + this.tytula.Text + "', '" + this.rezysera.Text + "', '" + this.roczeka.Text + "', '" + this.gatuneka.Text + "', '" + this.ocenaa.Text + "', '" + this.wyswietleniaa.Text + "', '" + this.obejrzanya.Text + "') ;";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                con.Open();
+                da.SelectCommand.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data saved successfully.");
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            new MainWindow().Show();
         }
     }
 }
--- Home.xaml.cs
+++ Home.xaml.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace GUI
 {
@@ -15,13 +16,6 @@ namespace GUI
         public Home()
         {
             InitializeComponent();
-            FillListView();
-
-        }
-        private ComboBox py1;
-
-        private void FillListView()
-        {
             SqlConnection con = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = MA; Integrated Security = True;");
             using SqlDataAdapter adapter = new SqlDataAdapter
             {
@@ -32,15 +26,9 @@ namespace GUI
             adapter.Fill(dt);
             dg.ItemsSource = dt.DefaultView;
 
-
         }
 
-        private void wyszukaj_Click(object sender, RoutedEventArgs e)
-        {
-            dg.Visibility = Visibility.Visible;
 
-            wyszukaj.Visibility = Visibility.Hidden;
-        }
 
 
         private void dg_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -58,7 +46,7 @@ namespace GUI
             views.Visibility = Visibility.Visible;
             rate.Visibility = Visibility.Visible;
             Watched.Visibility = Visibility.Visible;
-            Watchedtxt.Visibility = Visibility.Visible;
+            obejrzane.Visibility = Visibility.Visible;
             DataGrid dg = (DataGrid)sender;
             DataRowView row_selected = dg.SelectedItem as DataRowView;
             if (row_selected != null)
@@ -69,7 +57,7 @@ namespace GUI
                 gatunek.Text = row_selected["Genre"].ToString();
                 ocena.Text = row_selected["AvgRate"].ToString();
                 wyswietlenia.Text = row_selected["NumOfRates"].ToString();
-                Watchedtxt.Text = row_selected["Watched"].ToString();
+                obejrzane.Text = row_selected["Watched"].ToString();
 
             }
         }
@@ -77,10 +65,37 @@ namespace GUI
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Watchedtxt.Text = "True";
+            obejrzane.Text = "True";
 
         }
 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            /*CollectionViewSource Icollect = this.Resources["Icollect"] as CollectionViewSource;
+            Icollect.View.Filter = (obj) =>
+            {
+                System.Xml.XmlElement element = obj as System.Xml.XmlElement;
+                if (element.Attributes["Name"].Value.ToLower().StartsWith(filter.Text.Trim().ToLower()))
+                    return true;
+
+                return false;
+                */
+            SqlConnection con = new SqlConnection(@"Data Source =.\SQLEXPRESS; Initial Catalog = MA; Integrated Security = True;");
+            using SqlDataAdapter adapter = new SqlDataAdapter
+            {
+                SelectCommand = new SqlCommand("Select Title, Director, Year, Genre, AvgRate, NumOfRates, Watched  from movies", con)
+            };
+
+            DataTable dt = new DataTable("movies");
+            adapter.Fill(dt);
+            dg.ItemsSource = dt.DefaultView;
+
+            string filtr = filter.Text;
+                if (string.IsNullOrEmpty(filtr))
+                    dt.DefaultView.RowFilter = null;
+                else
+                    dt.DefaultView.RowFilter = string.Format("Title Like '%{0}%' OR Director Like '%{0}%'", filtr);
+            }
+        }
 
     }
-}
